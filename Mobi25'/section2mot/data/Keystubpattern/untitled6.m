f0928b8a8b9342cad0a70676b5c4a28b9b392c53
NEMO-LAB-repo <Nemo.lab02@gmail.com>
2025-03-25 23:08:18 -0400

    TEST1

--- Mobi25'/section2mot/data/Keystubpattern/untitled6.m
+++
@@ -1,8 +0,0 @@
-fid = fopen([path '/B-' num2str(kk) '.txt'], 'a+');%' num2str(ante) '
-for ii = 1:length(phase_diff)
-        fprintf(fid,'%f',data(ii)); 
-        fprintf(fid,'\t');
-        fprintf(fid,'\n');
-%         fclose(fid);
-end
-fclose(fid);    
